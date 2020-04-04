095882beb0c6d316dcb545c55f67dd829d65a0e1
Anton Lisikov <anton.lisikov@gmail.com>
2020-04-04 23:43:08 +0600

    feat: разделены шаблоны. Сделан отдельный шаблон для меню

--- winter/mainpage/views.py
+++ winter/mainpage/views.py
@@ -1,6 +1,15 @@
+"""
+    Mainpage app
+"""
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    """ Mainpage content
+    :param request:
+    :return:
+    """
+    content = {
+        'title': 'mainpage'
+    }
+    return render(request, 'index.html', content)
