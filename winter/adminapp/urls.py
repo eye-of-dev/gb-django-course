0d31841228b171a1a41d6da897d5e16ec7ff7937
Anton Lisikov <anton.lisikov@gmail.com>
2020-04-25 21:22:16 +0600

    feat: сделан раздел пользователей в админке. рефакторинг раздела категорий

--- winter/adminapp/urls.py
+++ winter/adminapp/urls.py
@@ -1,20 +1,28 @@
-import adminapp.views as adminapp
 from django.urls import path
 
-from .views import ProductsListView, ProductsCreateView, ProductsUpdateView, ProductsReadView, ProductsDeleteView
+from .views import ProductsListView, ProductsCreateView, ProductsUpdateView, ProductsReadView, ProductsDeleteView, \
+    UsersListView, UsersCreateView, UsersReadView, UsersUpdateView, UsersDeleteView, ProductsCategoriesListView, \
+    ProductsCategoriesCreateView, ProductsCategoriesReadView, ProductsCategoriesUpdateView, ProductsCategoriesDeleteView
 
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('product-categories/index', adminapp.product_categories_index, name='product_categories_index'),
-    path('product-categories/create', adminapp.product_categories_create, name='product_categories_create'),
-    path('product-categories/read/<int:pk>', adminapp.product_categories_read, name='product_categories_read'),
-    path('product-categories/update/<int:pk>', adminapp.product_categories_update, name='product_categories_update'),
-    path('product-categories/delete/<int:pk>', adminapp.product_categories_delete, name='product_categories_delete'),
+    path('product-categories/index', ProductsCategoriesListView.as_view(), name='product_categories_index'),
+    path('product-categories/create', ProductsCategoriesCreateView.as_view(), name='product_categories_create'),
+    path('product-categories/read/<int:pk>', ProductsCategoriesReadView.as_view(), name='product_categories_read'),
+    path('product-categories/update/<int:pk>', ProductsCategoriesUpdateView.as_view(), name='product_categories_update'),
+    path('product-categories/delete/<int:pk>', ProductsCategoriesDeleteView.as_view(), name='product_categories_delete'),
 
     path('products/index', ProductsListView.as_view(), name='products_list'),
     path('products/create', ProductsCreateView.as_view(), name='products_create'),
     path('products/read/<int:pk>', ProductsReadView.as_view(), name='products_read'),
     path('products/update/<int:pk>', ProductsUpdateView.as_view(), name='products_update'),
-    path('products/delete/<int:pk>', ProductsDeleteView.as_view(), name='products_delete')
+    path('products/delete/<int:pk>', ProductsDeleteView.as_view(), name='products_delete'),
+
+    path('users/index', UsersListView.as_view(), name='users_list'),
+    path('users/create', UsersCreateView.as_view(), name='users_create'),
+    path('users/read/<int:pk>', UsersReadView.as_view(), name='users_read'),
+    path('users/update/<int:pk>', UsersUpdateView.as_view(), name='users_update'),
+    path('users/delete/<int:pk>', UsersDeleteView.as_view(), name='users_delete')
+
 ]
